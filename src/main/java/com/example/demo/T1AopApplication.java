f755441aebdae7a58b97e0b685fe84ed24af914b
Кирилл Максимович <kirillking01@yandex.ru>
2024-10-23 06:35:14 +0300

    feat: добавить пакетную структуру проекта, необходимые зависимости, мейн-класс и gitignore

---
+++ src/main/java/com/example/demo/T1AopApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy
+@ComponentScan
+public class T1AopApplication {
+
+	public static void main(String[] args) {
+	}
+
+}
